29e1bb84480bc9bf259de7cc8aabcf4cbb75d8ac
Nick Schuch <nick@myschuch.com>
2015-02-21 06:13:39 -0500

    Account for directory issues

--- packer/scripts/install.sh
+++ packer/scripts/install.sh
@@ -3,5 +3,7 @@
 # Name: install.sh
 # Description: Install the results site.
 
+mv /tmp/results /var/www/results
 cd /var/www/results && bin/phing build
+chown -R www-data:www-data /var/www/results
 
--- packer/scripts/puppet.sh
+++ packer/scripts/puppet.sh
@@ -4,6 +4,6 @@
 # Author:      Nick Schuch (nick@myschuch.com)
 # Description: Installs Puppet manifest.
 
-DIR='/var/www/results/puppet'
+DIR='/tmp/results/puppet'
 
 cd $DIR && sh scripts/provision.sh
